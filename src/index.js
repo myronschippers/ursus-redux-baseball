5ff41bc81315ed488f96b51e828077be7ea5add0
Myron <dubtwee@gmail.com>
2020-04-15 14:11:31 -0500

    setting up initial redux configuration

--- src/index.js
+++ src/index.js
@@ -2,4 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// import redux and react-redux dependencies
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+
+// create individual reducers
+const baseball = (state, action) => {
+  return state;
+};
+
+// configure our redux store
+const storeInstance = createStore(
+  combineReducers({
+    baseball
+  })
+);
+
+// provide redux store access to our App
+ReactDOM.render(
+  <Provider store={storeInstance}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
